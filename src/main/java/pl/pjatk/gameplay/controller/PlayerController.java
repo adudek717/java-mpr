7c75bc2185b1613e7991157677e867760db11eb5
adudek7 <a.dudek717@gmail.com>
2020-11-28 17:34:15 +0100

    zjazd 4

--- src/main/java/pl/pjatk/gameplay/controller/PlayerController.java
+++ src/main/java/pl/pjatk/gameplay/controller/PlayerController.java
@@ -1,13 +1,12 @@
 package pl.pjatk.gameplay.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.pjatk.gameplay.model.Player;
 import pl.pjatk.gameplay.service.PlayerService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/player")
@@ -23,4 +22,40 @@ public class PlayerController {
     public ResponseEntity<List<Player>> findAll(){
         return ResponseEntity.ok(playerService.findAll());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Player> findById(@PathVariable Long id) {
+        Optional<Player> optionalPlayer = playerService.findByID(id);
+
+        if (optionalPlayer.isPresent()) {
+            return ResponseEntity.ok(optionalPlayer.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id){
+        playerService.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Player> save(@RequestBody Player player){
+        return ResponseEntity.ok(playerService.save(player));
+    }
+
+    @PutMapping
+    public ResponseEntity<Player> update(@RequestBody Player player){
+        return ResponseEntity.ok(playerService.update(player));
+    }
+
+    @GetMapping("/attack/{attackerId}/{defenderId}")
+    public ResponseEntity<Player> attackPlayer(@PathVariable Long attackerId, @PathVariable Long defenderId){
+        return ResponseEntity.ok(playerService.attackPlayer(attackerId,defenderId));
+    }
+
+
+    // Utwrz nowy mapping, w ktrym przyjmiemy id 2 graczy
+    // i grasz numer 1 zaatakuje gracza numer 2
 }
--- src/main/java/pl/pjatk/gameplay/service/PlayerService.java
+++ src/main/java/pl/pjatk/gameplay/service/PlayerService.java
@@ -2,21 +2,58 @@ package pl.pjatk.gameplay.service;
 
 import org.springframework.stereotype.Service;
 import pl.pjatk.gameplay.model.Player;
+import pl.pjatk.gameplay.repository.PlayerRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PlayerService {
 
-    public List<Player> findAll() {
-        List<Player> playerList = new ArrayList<>();
-        Player player = new Player();
-        Player player2 = new Player();
-        player.setAttack(100);
-        playerList.add(player);
-        playerList.add(player2);
-        return playerList;
+    private PlayerRepository playerRepository;
+    private DamageService damageService;
 
+    public PlayerService(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
+    public List<Player> findAll() {
+        return playerRepository.findAll();
+    }
+
+    public Optional<Player> findByID(Long playerId) {
+        return playerRepository.findById(playerId);
+    }
+
+    public Player save(Player player) {
+        return playerRepository.save(player);
+    }
+
+    public void delete(Player player) {
+        playerRepository.delete(player);
+    }
+
+    public void deleteById(Long id) {
+        playerRepository.deleteById(id);
+    }
+
+    public Player update(Player player) {
+        Optional<Player> byId = playerRepository.findById(player.getId());
+        if(byId.isEmpty()) {
+            throw new RuntimeException();
+        }
+        else {
+            return playerRepository.save(player);
+        }
+    }
+
+    public Player attackPlayer(Long attackerId, Long defenderId){
+        Player attacker = findByID(attackerId).get();
+        Player defender = findByID(defenderId).get();
+
+        defender = damageService.attackPlayer(attacker, defender);
+        return update(defender);
     }
 }
+
